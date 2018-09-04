28f28cffa9b3ed36b3c7d59985a11391344ceea1
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-04 18:40:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -13,11 +13,11 @@ def hopper
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-				:yukihiro_matsumoto => {
-				   :known_for => "Ruby",
-				   :languages => ["LISP", "C"]
-				}
+        }
+				#:yukihiro_matsumoto => {
+				#   :known_for => "Ruby",
+				#   :languages => ["LISP", "C"]
+				#}
      }
 		 programmer_hash[:grace_hopper]
 
