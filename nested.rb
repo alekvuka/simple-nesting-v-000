59361d2f00509d3efe882d56773fa55ec2767677
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-04 18:37:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -114,7 +114,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for] = "GUI"
 
-
 end
 
 def adding_to_dennis
@@ -133,5 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash =[:dennis_ritchie][:languages].push("Assembly")
 
 end
